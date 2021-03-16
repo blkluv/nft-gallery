0c284ced1cdabde36e28806e0abddab0f5e89398
Hassan Terry <hardworker1002@King-Hahz.attlocal.net>
2021-03-16 11:52:07 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# nft-gallery
+# 📱 Apps
+
+Apps are core components of [KodaDot wallet](https://twitter.com/KodaDot).
+
+Basic usage is to interact from browser with Polkadot and Substrate based networks.
+
+## 🐦 [Follow us on Twitter @KodaDot](https://twitter.com/KodaDot)
+
+## ▶️ Demo versions
+* 👩‍✈️[Stable release - could be used for production](https://kodadot.netlify.app/)
+* [🚧👷‍♀️ Development preview release - may not work everytime](https://dev-kodadot.netlify.app/)
+
+## 🕹 Play
+
+```shell
+git clone git@github.com:kodadot/nft-gallery.git
+cd dashboard;yarn
+yarn serve
+open http://localhost:8080/
+```
+
+## 🏦 Stage One
+* [Accounts](https://kodadot.netlify.app/#/accounts) - It offers basic management functionality to work with Accounts
+* [Address book](https://kodadot.netlify.app/#/addressbook) - It offers basic management functionality to work with Addresses
+* [Democracy](https://kodadot.netlify.app/#/democracy) - It offers basic voting app, allowing votes on activate proposals and referenda.
+* [Extrinsics](https://kodadot.netlify.app/#/extrinsics) - Extrinsics page is enviroment where user is able to execute every available transaction on the selected node.
+* [Transfer](https://kodadot.netlify.app/#/transfer) - Transfer function is abstraction on top of extrinsics for transfering various assets of network.
+* [Settings](https://kodadot.netlify.app/#/settings) - It provide seamless integration of vue-settings and vue-api inside Vuex, allowing choice of language, node to connect to, and theme.
+
+## 🏯 Stage Two
+* [Explorer](https://kodadot.netlify.app/#/explorer) - Explore chain name, best block, finalized blocks, total isuance, number of validators, state of epoch & era, deep dive in too finalized blocks, see hash of parent block, extrinsics and state. Next to it explore events happening in extrinsics, i.e. treasury deposit, balance transfers, council votes, democracy events and various governance drive
+* [ChainState - Storage](https://kodadot.netlify.app/#/chainstate) - Find out what's in Storage of chain, what are constants and raw inputs. Simple node query application, multiple queries can be queued and updates as new values become available
+* [ToolBox](https://kodadot.netlify.app/#/toolbox) - Utility hashing functions like Sign, Verify with accounts and hash data. 
+* [RPC](https://kodadot.netlify.app/#/rpc) - Submission of raw data to RPC endpoints
+* [Treasury](https://kodadot.netlify.app/#/treasury) - Vote on proposals and send tips 
+* 🏗 [Staking](https://kodadot.netlify.app/#/staking) - Staking managment, you can stake and nominate
+
+## 🏗 Development
+
+Contribution is welcome!
+
+We are using `yarn` workspace, as installing things via npm **will result in broken dependencies.**
+
+### 👷‍♀️ Get Started
+
+1. Clone the repo `git clone git@github.com:kodadot/nft-gallery.git <optional local path>`
+2. Ensure that you have a recent LTS version of [Node.js](https://nodejs.org/en/), we are using in time of writing version `v12.16.0`
+3. Recent version of [Yarn](https://yarnpkg.com/docs/install), we are now using `1.22.4`
+4. Install the dependencies by running `yarn`
+5. Now you can launch the UI. You can have local running [Substrate node](https://substrate.dev/docs/en/tutorials/start-a-private-network-with-substrate) or connect to existing networks
+6. Access the UI at [http://localhost:8080/](http://localhost:8080/)
+
+## 🙋‍♀️ I want to contribute
+
+Sure, your contribution is welcome. Please follow [code of conduct](CODE_OF_CONDUCT.md) and [contribution guidelines](CONTRIBUTING.md)
+
+
+## 🐳 Docker
+If you want just to try out our KodaDot on Kusama and have full local setup with local node, we assume you have [docker](https://docs.docker.com/get-docker/) and docker-compose installed. We have are building [images from develop and master branch](https://hub.docker.com/r/yangwao/kodadot/tags?page=1&ordering=last_updated)
+
+You need to make yourself be in `/dashboard` directory first
+```
+docker-compose pull && docker-compose up
+```
+
+If you want to run just KodaDot
+```
+docker-compose up kodadot
+```
+
+Build docker image of KodaDot
+```
+docker build -t hello/kodadot .
+```
+
+Run it locally and then visit `localhost:8080`
+```
+docker run -it -p 8080:8080 --rm --name hellokodadot hello/kodadot
+```
---
+++ dashboard/src/components/landing/Landing.vue
@@ -0,0 +1,198 @@
+<template>
+  <div>
+    <section class="hero is-light">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1">
+            <router-link :to="{ name: 'nft' }">
+              <b-icon
+                pack="fab"
+                icon="wpexplorer"
+                size="is-medium">
+              </b-icon>
+              KodaDot NFT  {{ $t('Explorer') }}<br>
+            </router-link>
+          </h1><br>
+          <div class="tile is-ancestor">
+            <div class="tile is-parent is-vertical">
+              <div class="tile is-child">
+                <h2 class="title is-3">
+                  <b-icon
+                    pack="fas"
+                    icon="camera-retro">
+                  </b-icon>
+                    <router-link :to="{ name: 'nftRare', }">
+                      Rare solo feed
+                    </router-link>
+                </h2>
+                <h2 class="title is-3">
+                  <b-icon
+                    pack="fas"
+                    icon="ghost"
+                  ></b-icon>
+                    Creators Spotlight
+                </h2>
+                <h2 class="subtitle is-4">
+                  <li
+                    v-for="c in creators"
+                    :key="c">
+                      <router-link :to="{ name: 'profile', params: { id: c } }">
+                        {{ c | shortAddress }}
+                      </router-link>
+                    </li>
+                </h2>
+              </div>
+              <div class="tile is-child">
+                <h2 class="title is-3">
+                  <b-icon
+                    pack="fab"
+                    icon="telegram-plane"
+                  ></b-icon>
+                    Community
+                </h2>
+                <h2 class="subtitle is-4">
+                  <li
+                    v-for="community in publicCommunity"
+                    :key="community[0]">
+                      <a :href="community[1]">{{ community[0] }}</a>
+                  </li>
+                </h2>
+              </div>
+            </div>
+              <div class="tile is-parent is-vertical">
+                <div class="tile is-child">
+                  <h2 class="title">
+                    <b-icon icon="flask"></b-icon>
+                    We are wandering new avenues of NFT experience.<br>
+                  </h2>
+                  <h2 class="title">
+                    <b-icon icon="leaf"></b-icon>
+                    Low carbon NFT platform running on
+                    <a href="https://kusama.network">Kusama.network</a> - NPoS
+                  </h2>
+                </div>
+                <div class="tile is-child">
+                  <h2 class="title is-3">
+                  <router-link :to="{ name: 'nft' }">
+                    <b-icon
+                      icon="eye">
+                    </b-icon>
+                    {{ $t('Gallery of NFT Tokens and Collectibles') }}
+                  </router-link>
+                </h2>
+                <h2 class="title is-4">
+                  <router-link :to="{ name: 'rmrk' }">
+                    <b-icon
+                      icon="print">
+                    </b-icon>
+                    {{ $t('Create your NFT Token & Collectibles') }}
+                  </router-link>
+                </h2><br>
+              </div>
+              <div class="tile is-child">
+                <h2 class="title is-4">
+                  <router-link :to="{ name: 'rmrkFaq' }">
+                    <b-icon
+                      icon="question">
+                    </b-icon>
+                    {{ $t('New here? We have F.A.Q.') }}
+                  </router-link>
+                </h2>
+                <h2 class="title is-4">
+                  <a href="https://github.com/kodadot/nft-gallery/issues/new" target="_blank">
+                    <b-icon
+                      pack="far"
+                      icon="comment-dots">
+                    </b-icon>
+                    {{ $t('Feedback? We\\\'d be happy to discuss it.') }}
+                  </a>
+                </h2><br>
+                  <h2 class="title">
+                    <a href="https://twitter.com/KodaDot" target="_blank">
+                      <b-icon
+                        pack="fab"
+                        icon="twitter">
+                      </b-icon>
+                      KodaDot
+                    </a>
+                  </h2>
+                  <h2 class="title">
+                    <a href="https://www.reddit.com/r/KodaDot/" target="_blank">
+                      <b-icon
+                        pack="fab"
+                        icon="reddit-alien">
+                      </b-icon>
+                      r/KodaDot
+                    </a>
+                  </h2>
+                  <h2 class="title">
+                    <a href="https://medium.com/kodadot/kodadot-nft-explorer-f2c3a326a856" target="_blank">
+                      <b-icon
+                        pack="fab"
+                        icon="medium">
+                      </b-icon>
+                      {{ $t('Read our story, how we started.') }}
+                    </a>
+                  </h2>
+                  <h2 class="title">
+                    <a href="https://medium.com/kodadot/traverse-to-the-prime-show-733d6046d3f5" target="_blank">
+                      <b-icon
+                        pack="fab"
+                        icon="medium">
+                      </b-icon>
+                      {{ $t('Traverse to the prime show') }}
+                    </a>
+                  </h2>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component({})
+export default class Landing extends Vue {
+  public publicCommunity: any = [
+    ['Bengali', 'https://t.me/KodaDot_Bengali'],
+    ['Czech', 'https://t.me/joinchat/Fhnvbi5a_wRjNzFk'],
+    ['Japan', 'https://t.me/joinchat/-CjTUgAflGQxYTU0'],
+    ['Korea', 'https://t.me/KodaDotKR'],
+    ['Portuguese', 'https://t.me/joinchat/1UHYFZpVYmE1OTZk'],
+    ['Poland', 'https://t.me/joinchat/HG7J2RAk906N7scb'],
+    ['Russia', 'https://t.me/kodadotru'],
+    ['Spanish','https://t.me/joinchat/HkF3cxImJAJGoRH9'],
+    ['Vietnamese', 'https://t.me/joinchat/GR0OiagewrkTzD4u'],
+    ['Want to help translate?', 'https://github.com/kodadot/nft-gallery/tree/i18n/dashboard/src/locales']
+  ]
+
+  public creators: any = [
+    'HtRTwHSP6fYC5PtCsJ7pG4H1hwyPhzXbtVTTVRJ6kvfPFe1',
+    'Dx6nVUy6f2znn4ZwNZ3TGbEyUz3FLbCRGQGKAut4LxjCVRs',
+    'J6mwrrDE5ZywDe8T4mWgsdgHCcrDcrVtoGh37i7PSaZARRs',
+    'DmUVjSi8id22vcH26btyVsVq39p8EVPiepdBEYhzoLL8Qby',
+    'CxQJNQeowRUJBgUHQpspudWy4BU6Rk2u93yDZEKe6GzcLDe',
+  ]
+
+}
+</script>
+
+<style scoped>
+.subtitle {
+  text-decoration: underline;
+}
+
+.truncate {
+  flex: 1;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+li {
+  list-style-type: square;
+}
+</style>
